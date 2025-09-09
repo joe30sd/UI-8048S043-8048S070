3806eac6a9512788c107a93e1ceff207e12ba645
joe30sd <joe30sd@gmail.com>
2025-09-09 16:17:39 -0400

    Fixed merge issue

--- src/main.cpp
+++ src/main.cpp
@@ -43,6 +43,7 @@ const int chipSelect = 10;
 
 //#define SELF_TEST_ON
 int minutesPassed = 0, o_timer = 0 ;
+unsigned long park_mode_start_time = 0, last_timestamp_display = 0;
 float live_magnet_current = 0, live_magnet_voltage = 0, magnet_current = 0, magnet_voltage = 0, resistor_value = 0;
 float main_voltage = 0, main_current = 0, att_current = 0, rda_current = 0, rda_voltage = 0, att_voltage = 0,ts_voltage = 0, c_clb1 = 0, c_clb2 = 0 , c_clb3 = 0, v_clb1 = 0;
 int self_tester = 17,self_tester_counter = 0, display_mode_flag = 0,rdf_flag = 0;
